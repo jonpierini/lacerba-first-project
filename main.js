40a7cf411a9411b9dc271a0394757626d6829ae6
jonpierini <pierinijonathan@gmail.com>
2023-04-28 13:01:36 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Hello world");
+let myInterface = {
+    myNumber: 10,
+    myString: "Hello world",
+};
+console.log('myNumber = ', myInterface.myNumber);
+console.log('myString =', myInterface.myString);
---
+++ main.ts
@@ -0,0 +1,15 @@
+console.log("Hello world");
+
+interface MyInterface {
+    myNumber: number;
+    myString: string;
+}
+
+let myInterface : MyInterface = {
+    myNumber: 10,
+    myString: "Hello world",
+
+};
+
+console.log('myNumber = ', myInterface.myNumber);
+console.log('myString =', myInterface.myString);
